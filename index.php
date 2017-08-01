4b559fb737ffa738532aa000b5f534712c2e7420
Dmitry Kubarko <dima-kubarko@yandex.by>
2017-08-01 13:47:10 +0300

    First commit. Header

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>БЛОГ_DK</title>
+    <link href="fonts/font-style.css" rel="stylesheet" type="text/css">
+    <link href="styles/style.css" rel="stylesheet">
+    <style>
+        nav ul li:first-child a{
+            background-color: #cecece;
+            color: #2f2f2f;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <p>BLOG_DK</p>
+            <nav>
+                <ul>
+                    <li><a href="/">БЛОГ</a></li>
+                    <li><a href="#">ГАЛЛЕРЕЯ</a></li>
+                    <li><a href="#">ОБО МНЕ</a></li>
+                </ul>
+            </nav>
+        </header>
+        <article></article>
+        <footer></footer>
+    </div>
+</body>
+
+</html>

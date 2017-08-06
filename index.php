c0a2881f7249d34da4aa7f7dd1bf06bb91b21fc8
Dmitry Kubarko <dima-kubarko@yandex.by>
2017-08-06 17:29:05 +0300

    Adaptive Header-Menu, Adaptive Footer

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href="fonts/font-style.css" rel="stylesheet" type="text/css">
     <link href="styles/style.css" rel="stylesheet">
     <style>
-        nav ul li:first-child a{
+        nav ul li:first-child a {
             background-color: #cecece;
             color: #2f2f2f;
         }
@@ -21,13 +21,28 @@
             <nav>
                 <ul>
                     <li><a href="/">БЛОГ</a></li>
-                    <li><a href="#">ГАЛЛЕРЕЯ</a></li>
+                    <li><a href="#">ГАЛЕРЕЯ</a></li>
                     <li><a href="#">ОБО МНЕ</a></li>
                 </ul>
             </nav>
         </header>
         <article></article>
-        <footer></footer>
+        <footer>
+            <div class="name-wrapper"><span>&copy;</span>
+                <div class="name">
+                    <p>Кубарко Дмитрий</p>
+                    <p><a href="#" title="Напишите мне">dima-kubarko@yandex.by</a></p>
+                </div>
+            </div>
+            <div class="social">
+                <a class="twitter-button-black" href="https://twitter.com/DzmitryOficial" target="_blank"></a>
+                <a class="twitter-button-white" href="https://twitter.com/DzmitryOficial" target="_blank"></a>
+                <a class="instagram-button-black" href="https://www.instagram.com/dkubarko/" target="_blank"></a>
+                <a class="instagram-button-white" href="https://www.instagram.com/dkubarko/" target="_blank"></a>
+                <a class="vk-button-black" href="https://vk.com/dkubarko" target="_blank"></a>
+                <a class="vk-button-white" href="https://vk.com/dkubarko" target="_blank"></a>
+            </div>
+        </footer>
     </div>
 </body>
 
